bade8e8f6b0a715b8a706cfd0cf01596e21c3c73
bassem <basem12683@gmail.com>
2023-11-01 01:08:07 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [cosmotics1, setCosmotics1] = useState("");
+  console.log(cosmotics1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-check form-check-inline">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio1"
+          value="option1"
+          onChange={(e) => setCosmotics1(e.target.value)}
+        />
+        <label className="form-check-label" htmlFor="inlineRadio1">
+          1
+        </label>
+      </div>
+      <div className="form-check form-check-inline">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio2"
+          value="option2"
+          onChange={(e) => setCosmotics1(e.target.value)}
+
+        />
+        <label className="form-check-label" htmlFor="inlineRadio2">
+          2
+        </label>
+      </div>
+      <div className="form-check form-check-inline">
+        <input
+          className="form-check-input"
+          type="radio"
+          name="inlineRadioOptions"
+          id="inlineRadio3"
+          value="option3"
+          onChange={(e) => setCosmotics1(e.target.value)}
+
+        />
+        <label className="form-check-label" htmlFor="inlineRadio3">
+          3 (disabled)
+        </label>
+      </div>
     </div>
   );
 }
