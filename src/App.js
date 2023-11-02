82af1f3bb644d2ccb23d7c931877a6a484af248e
bassem <basem12683@gmail.com>
2023-11-02 09:57:19 +0300

    data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { CompanyDb, skinKindDb } from "./CosmoticData";
+import { Fragment } from "react";
+import { productDb } from "./mederma.products";
+import { useEffect } from "react";
 
 function App() {
   const [checkedValue, setCheckedValue] = useState([]);
+  const [items, setItems] = useState([]);
 
   const handleChange = (e) => {
     // console.log(e.target.checked, "e.target.checked");
@@ -17,26 +22,59 @@ function App() {
   };
   console.log(checkedValue);
 
+  useEffect(() => {
+    if (!checkedValue) setItems(productDb);
+    setItems((_) =>
+      productDb.filter((x) =>
+        x.Description.includes(checkedValue)
+      )
+    );
+  }, [checkedValue]);
+
   return (
     <div className="container">
-      <h2>skills</h2>
-      <div>
-        <label className="">
-          <input type="checkbox" value="php" onChange={handleChange} />
-          PHP
-        </label>
-        <br />
-        <label className="">
-          <input type="checkbox" value="js" onChange={handleChange} />
-          JS
-        </label>
-        <br />
-        <label className="">
-          <input type="checkbox" value="react" onChange={handleChange} />
-          REACT
-        </label>
-      </div>
-      <div>{checkedValue}</div>
+      <div className="bg-warning">{checkedValue}</div>
+      <section>
+        <h2>company</h2>
+        {CompanyDb.map((s, i) => (
+          <Fragment key={i}>
+            {" "}
+            <label className="me-2">
+              <input
+                type="checkbox"
+                value={s.name}
+                onChange={handleChange}
+                className="me-1"
+              />
+              <span>{s.name}</span>
+            </label>
+          </Fragment>
+        ))}
+      </section>
+      <hr />
+      {/* ---------start test 2--------*/}
+      <section>
+        <h2>skin kind</h2>
+        {skinKindDb.map((s, i) => (
+          <Fragment key={i}>
+            {" "}
+            <label className="me-2">
+              <input
+                type="checkbox"
+                value={s.name}
+                onChange={handleChange}
+                className="me-1"
+              />
+              <span>{s.name}</span>
+            </label>
+          </Fragment>
+        ))}
+      </section>
+      {/* ---------start show product--------*/}
+      <hr />
+      {items.map((p, i) => (
+        <h5 key={i}>{p.Description}</h5>
+      ))}
     </div>
   );
 }
---
+++ src/CosmoticData.js
@@ -0,0 +1,298 @@
+export const useDb = [
+  {
+    id: 1,
+    name: "Anti Aging",
+  },
+  {
+    id: 2,
+    name: "Moisturization",
+  },
+  {
+    id: 3,
+    name: "Sunscreen",
+  },
+  {
+    id: 4,
+    name: "Pigmentation",
+  },
+  {
+    id: 5,
+    name: "Intim Protect",
+  },
+  {
+    id: 6,
+    name: "Deodorant",
+  },
+  {
+    id: 7,
+    name: "Cleanser",
+  },
+  {
+    id: 8,
+    name: "Scrubs",
+  },
+  {
+    id: 9,
+    name: "Acne",
+  },
+  {
+    id: 10,
+    name: "Pores",
+  },
+];
+
+export const CompanyDb = [
+  {
+    id: 1,
+    name: "BIODERMA",
+    ourPro:'https://www.bioderma.sa/our-products/'
+  },
+  {
+    id: 2,
+    name: "EUCERIN",
+    ourPro:'https://www.en.eucerin-me.com/products/'
+  },
+];
+
+export const companyCategoryDb = [
+  {
+    id: 1,
+    name: "SEBIUM",
+  },
+  {
+    id: 2,
+    name: "PHOTODERM",
+  },
+  {
+    id: 3,
+    name: "ATODERM",
+  },
+  {
+    id: 4,
+    name: "SENSIBIO",
+  },
+  {
+    id: 5,
+    name: "PIGMENTBIO",
+  },
+  {
+    id: 6,
+    name: "CICABIO",
+  },
+  {
+    id: 7,
+    name: "HYALURON FILLER",
+  },
+  {
+    id: 8,
+    name: "UREAREPAIR",
+  },
+  {
+    id: 9,
+    name: "SUN",
+  },
+  {
+    id: 10,
+    name: "DERMOPURIFYER",
+  },
+  {
+    id: 11,
+    name: "EVEN PIGMENT",
+  },
+  {
+    id: 12,
+    name: "PH5",
+  },
+  {
+    id: 13,
+    name: "AQUAPORIN",
+  },
+  {
+    id: 14,
+    name: "AQUAPHOR",
+  },
+  {
+    id: 15,
+    name: "ATOPICONTROL",
+  },
+  {
+    id: 16,
+    name: "ANTITRANSPIRANT",
+  },
+];
+
+export const compProTypeDb = [
+  {name:"intensive-baume"},
+  {name:"baume-levres"},
+  {name:"creme"},
+  {name:"gel-douche"},
+  {name:"huile-de-douche"},
+  {name:"intensive-gel-moussant"},
+  {name:"intensive-pain"},
+  {name:"mains-ongles"},
+  {name:"nutritive"},
+  {name:"sos-spray"},
+  {name:"stick-levres"},
+  {name:"aquafluide-spf50"},
+]
+
+export const formDb = [
+  {
+    id: 1,
+    name: "cream",
+  },
+  {
+    id: 2,
+    name: "ointment",
+  },
+  {
+    id: 3,
+    name: "solution",
+  },
+  {
+    id: 5,
+    name: "soap",
+  },
+  {
+    id: 6,
+    name: "balm",
+  },
+  {
+    id: 7,
+    name: "gel",
+  },
+  {
+    id: 8,
+    name: "lip stick",
+  },
+  {
+    id: 9,
+    name: "lotion",
+  },
+  {
+    id: 10,
+    name: "oil",
+  },
+];
+export const usedAreaDb = [
+  {
+    id: 1,
+    name: "Hand",
+  },
+  {
+    id: 2,
+    name: "Face",
+  },
+  {
+    id: 3,
+    name: "Foot",
+  },
+  {
+    id: 4,
+    name: "Eye",
+  },
+  {
+    id: 5,
+    name: "Body",
+  },
+  {
+    id: 6,
+    name: "Lip",
+  },
+  {
+    id: 7,
+    name: "Vaginal",
+  },
+];
+
+export const updateFormDb = [
+  {
+    id: 1,
+    text: "Company",
+    ping: [
+      {
+        id: 1,
+        name: "BIODERMA",
+      },
+      {
+        id: 2,
+        name: "EUCERIN",
+      },
+    ],
+  },
+];
+
+export const skinKindDb = [
+  {
+    id: 1,
+    name: "skinSenstivety",
+    option1: "no skin Senstivety",
+    option2: "Senstive",
+  },
+  {
+    id: 2,
+    name: "normalSkin",
+    option1: "no Normal Skin",
+    option2: "Normal",
+  },
+  {
+    id: 3,
+    name: "drySkin",
+    option1: "no Dry Skin",
+    option2: "Dry",
+  },
+  {
+    id: 4,
+    name: "oilySkin",
+    option1: "no Oily Skin",
+    option2: "Oily",
+  },
+  {
+    id: 5,
+    name: "combinationSkin",
+    option1: "no Combination Skin",
+    option2: "Combination",
+  },
+  {
+    id: 6,
+    name: "atopicSkin",
+    option1: "no Atopic Skin",
+    option2: "Atopic",
+  },
+  {
+    id: 7,
+    name: "aknePoreSkin",
+    option1: "no AknePore Skin",
+    option2: "AknePore",
+  },
+  {
+    id: 8,
+    name: "hyperpigmentedSkin",
+    option1: "no HyperPigmented Skin",
+    option2: "HyperPigmented",
+  },
+  {
+    id: 9,
+    name: "flushedSkin",
+    option1: "no Flushed Skin",
+    option2: "Flushed",
+  },
+  {
+    id: 10,
+    name: "irritatedSkin",
+    option1: "no Irritated Skin",
+    option2: "Irritated",
+  },
+  {
+    id: 11,
+    name: "damagedSkin",
+    option1: "no Damaged Skin",
+    option2: "Damaged",
+  },
+];
+
+export const freeDb = [
+  { name: "soapFree" },
+  { name: "paraffinFree" },
+  { name: "fregranceFree" },
+];
